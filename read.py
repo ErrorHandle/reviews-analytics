5f4221443964d393fd715c0a1fd1baa95b5073b1
Error Handle <mazlol0311@gmail.com>
2019-07-24 15:40:05 +0800

    add word count function

--- read.py
+++ read.py
@@ -10,7 +10,33 @@ with open('reviews.txt', 'r') as f:
 
 print('檔案讀取完了，總共有', len(data),'筆資料')
 
-sum_len = 0
+wc = {}
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+for word in wc:
+	if wc[word] > 100:
+		print(word, wc[word])
+
+print(len(wc))
+
+while True:
+	word = input('Enter the word you want to search for: ')
+	if word not in wc:
+		print('error')
+		continue
+	if word == 'q':
+		break
+	else:
+		print(wc[word])
+
+
+
+'''sum_len = 0
 for d in data:
 	sum_len += len(d)
 
@@ -25,5 +51,16 @@ for d in data:
 		new.append(d)
 
 print('commit < len(100) :', len(new)) 
-print(new[0])
-print(new[1])
+
+check_string = 'good'
+checkbox = []
+
+#for d in data:
+#	if check_string in d:
+#		checkbox.append(d)
+#
+#print(len(checkbox))
+
+checkbox = [d + '123' for d in data if check_string in d]
+bad = ['bad' in d for d in data]
+print(bad)'''
